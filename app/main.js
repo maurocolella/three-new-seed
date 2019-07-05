09417bf12b2faefa80dd9e197ce5c39ddacf7749
Mauro Colella <mauro.ludovico.colella@gmail.com>
2019-07-06 01:06:05 +0800

    Add interactive labels

--- app/main.js
+++ app/main.js
@@ -1,6 +1,78 @@
 'use strict';
 
-(function(){
+(function () {
+  // https://tc39.github.io/ecma262/#sec-array.prototype.find
+  if (!Array.prototype.find) {
+    Object.defineProperty(Array.prototype, 'find', {
+      value: function (predicate) {
+        // 1. Let O be ? ToObject(this value).
+        if (this == null) {
+          throw new TypeError('"this" is null or not defined');
+        }
+
+        var o = Object(this);
+
+        // 2. Let len be ? ToLength(? Get(O, "length")).
+        var len = o.length >>> 0;
+
+        // 3. If IsCallable(predicate) is false, throw a TypeError exception.
+        if (typeof predicate !== 'function') {
+          throw new TypeError('predicate must be a function');
+        }
+
+        // 4. If thisArg was supplied, let T be thisArg; else let T be undefined.
+        var thisArg = arguments[1];
+
+        // 5. Let k be 0.
+        var k = 0;
+
+        // 6. Repeat, while k < len
+        while (k < len) {
+          // a. Let Pk be ! ToString(k).
+          // b. Let kValue be ? Get(O, Pk).
+          // c. Let testResult be ToBoolean(? Call(predicate, T, « kValue, k, O »)).
+          // d. If testResult is true, return kValue.
+          var kValue = o[k];
+          if (predicate.call(thisArg, kValue, k, o)) {
+            return kValue;
+          }
+          // e. Increase k by 1.
+          k++;
+        }
+
+        // 7. Return undefined.
+        return undefined;
+      },
+      configurable: true,
+      writable: true
+    });
+  }
+
+  // Configuration
+  const payload = [
+    {
+      title: 'Google',
+      subtitle: 'Search engine',
+      link: 'https://www.google.com'
+    },
+    {
+      title: 'Bing',
+      subtitle: 'Search engine',
+      link: 'https://www.bing.com'
+    },
+    {
+      title: 'Amazon',
+      subtitle: 'Electronic store',
+      link: 'https://www.amazon.com'
+    }
+  ];
+
+  const fontStyle = {
+    font: 'Verdana, sans-serif',
+    color: '#777777',
+  };
+
+
   // Script globals
   let renderer,
     composer,
@@ -17,11 +89,15 @@
     mouse,
     raycaster,
     key,
-	shaderRefs,
+    shaderRefs,
     scramblerEnabled = 1,
-	randomSeed = 0;
-    
-  let tessellateModifier = new THREE.TessellateModifier( 1 );
+    randomSeed = 0,
+    animController = {
+      foldIntensity: 2
+    },
+    labelSprites = payload.map(function (entry) { return renderText(entry.title, entry.subtitle, fontStyle); });
+
+  let tessellateModifier = new THREE.TessellateModifier(1);
 
   // Shaders
   let bloomPass,
@@ -43,7 +119,7 @@
 
     // scene
     scene = new THREE.Scene();
-    scene.background = new THREE.Color( 0x1b1c1f /*0x18191d */ );
+    scene.background = new THREE.Color(0x1b1c1f /*0x18191d */);
 
     // camera
     camera = new THREE.PerspectiveCamera(
@@ -60,10 +136,10 @@
     controls.enableZoom = false;
     controls.enabled = false;
     controls.rotateSpeed = 0.18;
-	// controls.target = new THREE.Vector3(0, 0, 8);
-	
-	controls.minAzimuthAngle = -Math.PI / 2; // radians
-	controls.maxAzimuthAngle = Math.PI / 2; // radians
+    // controls.target = new THREE.Vector3(0, 0, 8);
+
+    controls.minAzimuthAngle = -Math.PI / 2; // radians
+    controls.maxAzimuthAngle = Math.PI / 2; // radians
 
     // Remove default OrbitControls event listeners
     controls.dispose();
@@ -80,8 +156,8 @@
     light = new THREE.DirectionalLight(0x9090a2);
     light.position.copy(camera.position);
     scene.add(light);
-	
-	shaderRefs = [];
+
+    shaderRefs = [];
 
     initGeometry();
 
@@ -89,31 +165,63 @@
     addComposer();
   }
 
-  function augmentShader( shader ) {
-	shader.uniforms.time = { value: 0 };
-	shader.uniforms.scramblerActive = { value: 0 };
-	shader.vertexShader = shader.vertexShader.replace(
-	  '#include <common>',
-	  '#include <common>' + getShader('seededNoise')
-	)
-	shader.vertexShader = shader.vertexShader.replace(
-	  '#include <begin_vertex>',
-	  getShader('vertexScrambler')
-	);
-	
-	shaderRefs.push(shader);
+  function augmentShader(shader) {
+    shader.uniforms.intensity = { value: animController.foldIntensity };
+    shader.uniforms.factor = { value: 0 };
+    shader.uniforms.time = { value: 0 };
+    shader.uniforms.scramblerActive = { value: 0 };
+    shader.vertexShader = shader.vertexShader.replace(
+      '#include <common>',
+      '#include <common>' + getShader('seededNoise')
+    )
+    shader.vertexShader = shader.vertexShader.replace(
+      '#include <begin_vertex>',
+      getShader('vertexScrambler')
+    );
+
+    shaderRefs.push(shader);
   };
+
+  function renderText(text, subtext, style, textureSize = 512) {
+    let canvas = document.createElement('canvas');
+    canvas.width = textureSize;
+    canvas.height = textureSize;
+
+    let context = canvas.getContext('2d');
+    context.font = '52px ' + style.font;
+    context.textBaseline = 'bottom';
+    context.textAlign = 'center';
+    context.fillStyle = style.color;
+    context.fillText(text.toUpperCase(), textureSize / 2, textureSize / 2);
+
+    context.font = '36px ' + style.font;
+    context.textBaseline = 'top';
+    context.fillText(subtext.toUpperCase(), textureSize / 2, textureSize / 2 + 10);
+
+    const texture = new THREE.Texture(canvas);
+    texture.needsUpdate = true;
+
+    const spriteMaterial = new THREE.SpriteMaterial({
+      map: texture,
+      fog: true,
+    });
+    const sprite = new THREE.Sprite(spriteMaterial);
+    context = null;
+    canvas = null;
+    sprite.name = text.toLowerCase();
+    return sprite;
+  }
 
   // Initialize geometry
   function initGeometry() {
     const cubeGeometry = new THREE.BoxGeometry(10, 10, 10);
-    
-	const pyramidGeometry = new THREE.ConeGeometry(4, 2, 3); // new THREE.FancyTetrahedronGeometry(4, 0);
-    const edges = new THREE.EdgesGeometry( pyramidGeometry );
-	tessellateModifier.modify(pyramidGeometry);
-    
-	const pointGeometry = new THREE.ConeGeometry(4, 2, 3);
-	
+
+    const pyramidGeometry = new THREE.ConeGeometry(4, 2, payload.length); // new THREE.FancyTetrahedronGeometry(4, 0);
+    const edges = new THREE.EdgesGeometry(pyramidGeometry);
+    tessellateModifier.modify(pyramidGeometry);
+
+    const pointGeometry = new THREE.ConeGeometry(4, 2, payload.length);
+
     const material = new THREE.MeshLambertMaterial({
       color: 0x222225, // 0x555558,
     });
@@ -137,17 +245,17 @@
     wirePyramidMesh = new THREE.Mesh(edges, lineMaterial);
     pointPyramidMesh = new THREE.Points(pointGeometry, pointMaterial);
 
-    cubeMesh.position.set(0,0,0);
-    cubeMesh.rotation.set(0.6,-0.3,0);
+    cubeMesh.position.set(0, 0, -4);
+    cubeMesh.rotation.set(0.6, -0.3, 0);
 
-    pyramidMesh.position.set(0,-2,0);
+    pyramidMesh.position.set(0, -2, -4);
     pyramidMesh.visible = false;
 
-    wirePyramidMesh.position.set(0,-2,0);
+    wirePyramidMesh.position.set(0, -2, -4);
     wirePyramidMesh.scale.set(1.1, 1.1, 1.1);
     wirePyramidMesh.visible = false;
 
-    pointPyramidMesh.position.set(0,-2,0);
+    pointPyramidMesh.position.set(0, -2, -4);
     pointPyramidMesh.scale.set(1.1, 1.1, 1.1);
     pointPyramidMesh.visible = false;
 
@@ -155,47 +263,55 @@
     scene.add(pyramidMesh);
     scene.add(wirePyramidMesh);
     scene.add(pointPyramidMesh);
+
+    let i;
+    for (i = 0; i < labelSprites.length; i++) {
+      labelSprites[i].scale.set(12.0, 12.0, 12.0);
+      labelSprites[i].visible = false;
+      labelSprites[i].onBeforeRender = function (renderer) { renderer.clearDepth(); };
+      scene.add(labelSprites[i]);
+    }
   }
 
   // ********************** Animation *********************
 
   function animate() {
     const time = performance.now() * 0.0001;
-	const span = time * 1000 % 300;
-	if (span < 70 && span % 10 < 1) {
-		randomSeed = Math.random();
-	}
-	
-	const prismRotation = 
-          {
-            x: Math.PI * 6.4,
-            y: -Math.PI * 8.1,
-            z: Math.PI * 4
-          };
+    const span = time * 1000 % 300;
+    if (span < 70 && span % 10 < 1) {
+      randomSeed = Math.random();
+    }
 
-    switch(key) {
+    const prismRotation =
+    {
+      x: Math.PI * 6.4,
+      y: -Math.PI * 8.1,
+      z: Math.PI * 4
+    };
+
+    switch (key) {
       case 'toPrism':
         // Animate cube
         (new TimelineLite())
-        .to(
-          cubeMesh.rotation,
-          2,
-          {
-            x: Math.PI * 5,
-            y: -Math.PI * 0.25,
-            z: Math.PI * 2 /* ,
+          .to(
+            cubeMesh.rotation,
+            2,
+            {
+              x: Math.PI * 5,
+              y: -Math.PI * 0.25,
+              z: Math.PI * 2 /* ,
             ease: Power4.easeIn */
-          }
-        ).to(
-          cubeMesh.scale,
-          1,
-          {
-            x: 0.4,
-            y: 0.4,
-            z: 0.4,
-          },
-          0
-        );
+            }
+          ).to(
+            cubeMesh.scale,
+            1,
+            {
+              x: 0.4,
+              y: 0.4,
+              z: 0.4,
+            },
+            0
+          );
 
         // Animate prism
         (new TimelineLite()).to(
@@ -206,9 +322,9 @@
           pyramidMesh.scale,
           1.5,
           {
-            x: 3.5,
-            y: 3.5,
-            z: 3.5 /* ,
+            x: 3.2,
+            y: 3.2,
+            z: 3.2 /* ,
             ease: Power4.easeIn */
           },
           0
@@ -223,9 +339,9 @@
           wirePyramidMesh.scale,
           1.5,
           {
-            x: 3.85,
-            y: 3.85,
-            z: 3.85 /* ,
+            x: 3.55,
+            y: 3.55,
+            z: 3.55 /* ,
             ease: Power4.easeIn */
           },
           0
@@ -235,48 +351,75 @@
         (new TimelineLite()).to(
           pointPyramidMesh.rotation,
           1.2,
-		  prismRotation
+          prismRotation
         ).to(
           pointPyramidMesh.scale,
           1.5,
           {
-            x: 3.85,
-            y: 3.85,
-            z: 3.85 /* ,
+            x: 3.55,
+            y: 3.55,
+            z: 3.55 /* ,
             ease: Power4.easeIn */
           },
           0
         ).add(
-          () => {
+          function () {
             pyramidMesh.visible = true;
             wirePyramidMesh.visible = true;
             pointPyramidMesh.visible = true;
             cubeMesh.visible = false;
-            controls.enabled = true;
-			// controls.autoRotate = true;
 
-			let i = 0;
-			for(;i<shaderRefs.length;i++){
-				shaderRefs[i].uniforms.scramblerActive.value = scramblerEnabled;
-			}
+            let i = 0;
+            for (; i < shaderRefs.length; i++) {
+              shaderRefs[i].uniforms.scramblerActive.value = scramblerEnabled;
+            }
           },
-        0.7);
-
-
+          0.7);
         break;
+      case 'unfold':
+        (new TimelineLite()).to(
+          animController,
+          1,
+          {
+            ease: Back.easeOut,
+            foldIntensity: 0
+          }
+        )
+          .add(
+            function () {
+              controls.enabled = true;
+              key = 'idle';
+
+              let i;
+              const vertices = pointPyramidMesh.geometry.vertices;
+              let vertex;
+
+              for (i = 0; i < labelSprites.length; i++) {
+                vertex = vertices[i + 1].clone();
+                vertex = new THREE.Vector3(vertex.x * 1.2, vertex.y * 1.2, vertex.z * 1.2);
+                vertex.applyMatrix4(pointPyramidMesh.matrix);
+                labelSprites[i].visible = true;
+                labelSprites[i].position.set(vertex.x, vertex.y, vertex.z);
+              }
+            }
+          );
+        let i = 0;
+        for (; i < shaderRefs.length; i++) {
+          shaderRefs[i].uniforms.intensity.value = animController.foldIntensity;
+        }
       default:
         break;
     }
 
-	if (scramblerEnabled) {
-		let i = 0;
-		for(;i<shaderRefs.length;i++){
-			shaderRefs[i].uniforms.time.value = randomSeed;
-		}
-	}
-    chromaticAberrationPass.uniforms["time"].value = Math.cos(time*10);
+    if (scramblerEnabled) {
+      let i = 0;
+      for (; i < shaderRefs.length; i++) {
+        shaderRefs[i].uniforms.factor.value = randomSeed;
+      }
+    }
+    chromaticAberrationPass.uniforms["time"].value = Math.cos(time * 10);
     light.position.copy(camera.position);
-	controls.update();
+    controls.update();
     requestAnimationFrame(animate);
     composer.render(time);
   }
@@ -355,14 +498,21 @@
     }
 
     // Object picking
-    mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
-    mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+    mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+    mouse.y = - (event.clientY / window.innerHeight) * 2 + 1;
 
     const objectsHovered = mouseIntersects();
 
     // Indicate an object is picked
     if (objectsHovered.length > 0) {
-      document.body.style.cursor = 'pointer';
+      let i = 0;
+      for (; i < objectsHovered.length; i++) {
+        if (objectsHovered[i].object.type === 'Sprite') {
+          // console.log('sprite hovered');
+          document.body.style.cursor = 'pointer';
+        }
+      }
+      // document.body.style.cursor = 'pointer';
     } else {
       document.body.style.cursor = 'auto';
     }
@@ -373,14 +523,27 @@
     const objectsPicked = mouseIntersects();
 
     if (objectsPicked.length > 0) {
-      key = 'toPrism';
+      if (!key) {
+        key = 'toPrism';
+      } else {
+        key = 'unfold';
+      }
+
+      let i = 0;
+      for (; i < objectsPicked.length; i++) {
+        if (objectsPicked[i].object.type === 'Sprite') {
+          const linkName = objectsPicked[i].object.name;
+          const result = payload.find(function(item) { return item.title.toLowerCase() === linkName });
+          window.open(result.link);
+        }
+      }
     }
   }
-  
+
   function onKeyUp(event) {
-	if(event.keyCode == 32){
-        //your code
-        scramblerEnabled = scramblerEnabled === 0 ? 1 : 0;
+    if (event.keyCode == 32) {
+      //your code
+      scramblerEnabled = scramblerEnabled === 0 ? 1 : 0;
     }
   }
 
